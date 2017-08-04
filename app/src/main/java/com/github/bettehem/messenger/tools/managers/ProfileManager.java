f4a1ddea5313554e2efef40f2b30c72e630aa5d0
Bettehem <chris.mustola@gmail.com>
2017-08-04 20:18:03 +0300

    removed redundant imports

--- app/src/main/java/com/github/bettehem/messenger/tools/managers/ProfileManager.java
+++ app/src/main/java/com/github/bettehem/messenger/tools/managers/ProfileManager.java
@@ -1,19 +1,9 @@
 package com.github.bettehem.messenger.tools.managers;
 
 import android.content.Context;
-import android.util.Log;
-import android.widget.Toast;
-
 import com.github.bettehem.androidtools.Preferences;
 import com.github.bettehem.messenger.tools.listeners.ProfileListener;
 import com.github.bettehem.messenger.tools.users.UserProfile;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import static android.content.ContentValues.TAG;
 
 public abstract class ProfileManager {
 
